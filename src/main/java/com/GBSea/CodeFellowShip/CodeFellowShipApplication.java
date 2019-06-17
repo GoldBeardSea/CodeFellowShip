314ca30a233da18cb82b5f45e20b8e31031bee53
Timothy Joseph Busch <timbusch1@gmail.com>
2019-06-17 15:05:26 -0700

    initial commit

---
+++ src/main/java/com/GBSea/CodeFellowShip/CodeFellowShipApplication.java
@@ -0,0 +1,13 @@
+package com.GBSea.CodeFellowShip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeFellowShipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeFellowShipApplication.class, args);
+	}
+
+}
